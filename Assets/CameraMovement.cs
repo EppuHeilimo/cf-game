28707733745f64266c736f7840ea4ec9f4c2e046
EppuHeilimo <eppu.heilimo@yahoo.com>
2016-05-17 12:19:42 +0300

    Add isometric sandbox with player following camera

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    Vector3 defaultPosition;
+	// Use this for initialization
+	void Start () {
+        defaultPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform playerTransform = GameObject.Find("Player").transform;
+        Vector3 playerPos = playerTransform.position;
+        Vector3 camPos = transform.position;
+        if(playerPos.x != camPos.x || playerPos.z != camPos.z)
+        {
+            transform.position = new Vector3(playerPos.x + defaultPosition.x, camPos.y, playerPos.z + defaultPosition.z);
+        }
+	}
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    float speed = 20.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z + speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z - speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z + speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z - speed * Time.deltaTime);
+        }
+    }
+}
