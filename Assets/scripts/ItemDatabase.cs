0601a65fba4fe917fc7265469309a0d15c24cda4
vuoale <H8346@student.jamk.fi>
2016-05-27 15:42:05 +0300

    Add inventory system

---
+++ Assets/scripts/ItemDatabase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour {
+
+    public List<Item> items = new List<Item>();
+
+	// Use this for initialization
+	void Start () {
+        items.Add(new Item("burana", 0, "särkylääke"));
+        items.Add(new Item("kokaiini", 1, "auttaa vaivaan kuin vaivaan"));
+	}
+
+}
