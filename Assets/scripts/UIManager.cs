ccacafc644dbbef79b3135a23469a8bc10e5577e
vuoale <H8346@student.jamk.fi>
2016-06-01 10:38:21 +0300

    Inventory toggle

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -5,16 +5,20 @@ public class UIManager : MonoBehaviour {
 
     GameObject player;
     GameObject target;
+    GameObject inv;
+    bool showInventory;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
+        inv = GameObject.FindGameObjectWithTag("Inventory");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetButtonDown("Inventory"))
+            toggleInventory();
+    }
 
     public bool giveMed(string med)
     {
@@ -24,5 +28,16 @@ public class UIManager : MonoBehaviour {
         else if (target.tag == "NPC")
             return(target.GetComponent<NPC>().giveMed(med));
         return false;
+    }
+
+    public void toggleInventory()
+    {
+        showInventory = !showInventory;
+        drawInventory(showInventory);
+    }
+
+    public void drawInventory(bool show)
+    {
+        inv.SetActive(show);
     }
 }
