80be72a425dd749ceef77d51297bb471f8d16315
vuoale <H8346@student.jamk.fi>
2016-06-02 11:15:47 +0300

    Add HP bars

--- Assets/scripts/TextBoxManager.cs
+++ Assets/scripts/TextBoxManager.cs
@@ -7,9 +7,12 @@ public class TextBoxManager : MonoBehaviour {
     [SerializeField]
     Text dialog;
 
+    GameObject healthBar;
+
     // Use this for initialization
     void Start()
     {
+        healthBar = GameObject.FindGameObjectWithTag("HpBar");
         DisableTextBox();
     }
 
@@ -24,11 +27,19 @@ public class TextBoxManager : MonoBehaviour {
         if (!medActive)
             medStatus = "Not active";
         dialog.text = "Name: " + myName + "\n" + "ID: " + myId + "\n" + "Hp: " + myHp.ToString() + "\n" + "Medicine: " + medStatus + "\n" + "Problem: " + myProblem;
+        SetHealthBar(myHp);
+        healthBar.SetActive(true);
     }
 
     public void DisableTextBox()
     {
+        healthBar.SetActive(false);
         dialog.text = null;
     }
 
+    public void SetHealthBar(float myHp)
+    {
+        float scaledHp = myHp / 100f;
+        healthBar.transform.localScale = new Vector3(Mathf.Clamp(scaledHp, 0f, 1f), healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+    }
 }
