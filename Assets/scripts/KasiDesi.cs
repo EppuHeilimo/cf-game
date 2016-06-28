0f998bd001d82b5c5fa6be14f65c041e64c39e08
vuoale <H8346@student.jamk.fi>
2016-06-28 16:20:22 +0300

    UI stuff

---
+++ Assets/scripts/KasiDesi.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class KasiDesi : MonoBehaviour {
+
+    Image image;
+    float timeLeft;
+    Color targetColor;
+    bool blink;
+    GameObject kasDesTxtObj;
+    Text kasDesTxt;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        kasDesTxtObj = GameObject.FindGameObjectWithTag("KasDesTxt");
+        kasDesTxt = kasDesTxtObj.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (blink)
+        {
+            image.color = new Color(255, 0, 0, (Mathf.Sin(Time.time * 2.0f) + 1.0f) / 2.0f);
+        }
+    }
+
+    public void StartBlinking()
+    {
+        kasDesTxt.text = "Click it!";
+        blink = true;
+    }
+
+    public void StopBlinking()
+    {
+        kasDesTxt.text = "";
+        blink = false;
+    }
+
+    public void SetDefaultColor()
+    {
+        image.color = new Color(255, 255, 255, 1);
+    }
+}
