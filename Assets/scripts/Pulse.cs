cd16318df9b91fe52c8b2adef9227c7d35d123da
vuoale <H8346@student.jamk.fi>
2016-07-08 13:44:43 +0300

    Main menu anim

---
+++ Assets/scripts/Pulse.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var scaleNormal = new GoTween(transform, 0.3f, new GoTweenConfig().scale(1f));
+        var scaleBig = new GoTween(transform, 0.3f, new GoTweenConfig().scale(1.2f));
+
+        var chain = new GoTweenChain();
+        chain.append(scaleBig).append(scaleNormal);
+        chain.play();
+        chain.setOnCompleteHandler((x) =>
+        {
+            x.restart();
+        });
+    }
+
+   
+}
