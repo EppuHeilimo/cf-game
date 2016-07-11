1083fc7d60cba22d76092a87007d83f8bdbcd0d7
vuoale <H8346@student.jamk.fi>
2016-07-11 14:32:29 +0300

    Main menu anims

--- Assets/scripts/Pulse.cs
+++ Assets/scripts/Pulse.cs
@@ -5,23 +5,10 @@ public class Pulse : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        var scaleNormal = new GoTween(transform, 0.3f, new GoTweenConfig().scale(1f));
-        var scaleBig = new GoTween(transform, 0.3f, new GoTweenConfig().scale(1.2f));
-
-        var chain = new GoTweenChain();
-        chain.append(scaleBig).append(scaleNormal);
-        chain.play();
-        chain.setOnCompleteHandler((x) =>
-        {
-            x.restart();
-        });
+        var tween = Go.to(transform, 0.3f, new GoTweenConfig()
+            .scale(new Vector3(1.2f, 1.2f, 1.2f))
+            .setIterations(-1, GoLoopType.PingPong));
     }
-
    
 }
